724a1a8acc70619261bb7b06117789e2836f88d1
Juarez <juarezoliveira107142@gmail.com>
2022-03-11 18:33:35 -0300

    Refatorei meu código e criei a classe Dice.

--- app.js
+++ app.js
@@ -1,37 +1,47 @@
+class Dice {
+    constructor(element, angles) {
+        this.element = element;
+        this.element.angles = angles; //Já que element é um objeto dá pra armazenar valores dentro dele.
+        this.element.addEventListener('click', this.randomize);
+        this.element.addEventListener('click', this.grow);
+        this.element.addEventListener('animationend', this.resetAnimation);
+    }
+
+    //Como esses métodos vão ser chamados por um evento o 'this' se refere ao alvo do evento, 
+    //nesse caso o element, e não à instância da classe.
+    
+    randomize() {
+        if (this.classList.contains('miniature')) return;
+        const randomAngle = this.angles[Math.floor(Math.random() * this.angles.length)];
+        const [x, y] = randomAngle;
+        this.style.animation = 'Roll 1.5s linear';
+        this.style.transform = `rotateX(${x}deg) rotateY(${y}deg)`;
+    }
+
+    //O método shrink vai ser chamado na instância da classe, então nesse caso é a ela que o 'this' se refere,
+    //e por isso é preciso usar 'this.element'.
+    shrink() {
+        this.element.classList.add('miniature');
+        showcase.append(this.element);
+        this.element.style.transform = '';
+    }
+
+    grow() {
+        if (!this.classList.contains('miniature')) return;
+        this.classList.remove('miniature');
+        container.append(this);
+        this.style.animation = 'PopIn 0.5s linear';
+    }
+
+    resetAnimation() {
+        this.style.animation = '';
+    }
+}
+
 const showcase = document.getElementById('showcase');
 const container = document.getElementById('container');
 
 const d6 = document.getElementById('d6');
-const d6Angles = [[0, 0], [0, 90], [90, 0], [270, 0], [0, 270], [0, 180]]
+const d6Angles = [[0, 0], [0, 90], [90, 0], [270, 0], [0, 270], [0, 180]];
 
-
-const randomize = function () {
-    const randomAngle = d6Angles[Math.floor(Math.random() * 6)];
-    const [x, y] = randomAngle;
-    d6.style.animation = 'Roll 1.5s linear';
-    d6.style.transform = `rotateX(${x}deg) rotateY(${y}deg)`;
-}
-
-const shrink = function (element) {
-    element.classList.add('miniature');
-    showcase.append(element);
-    element.style.transform = '';
-    element.removeEventListener('click', randomize);
-    element.addEventListener('click', () => grow(element));
-}
-
-const grow = function (element) {
-    element.classList.remove('miniature');
-    container.append(element);
-    element.addEventListener('click', randomize);
-    element.style.animation = 'PopIn 0.5s linear';
-}
-
-
-d6.addEventListener('click', randomize);
-
-d6.addEventListener('animationend', () => {
-    d6.style.animation = '';
-})
-
-shrink(d6);
+D6 = new Dice(d6, d6Angles);
