4b7d431b1b602d276190147bab76803f9a835fc0
Juarez <juarezoliveira107142@gmail.com>
2022-03-12 14:17:46 -0300

    Arrumei alguns erros, refatorei métodos da classe Dice

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ class Dice {
         this.element = element;
         this.element.angles = angles; //Já que element é um objeto dá pra armazenar valores dentro dele.
         this.element.addEventListener('click', this.randomize);
-        this.element.addEventListener('click', this.grow);
+        this.element.addEventListener('click', this.changeDice);
         this.element.addEventListener('animationend', this.resetAnimation);
     }
 
@@ -18,16 +18,15 @@ class Dice {
         this.style.transform = `rotateX(${x}deg) rotateY(${y}deg)`;
     }
 
-    //O método shrink vai ser chamado na instância da classe, então nesse caso é a ela que o 'this' se refere,
-    //e por isso é preciso usar 'this.element'.
-    shrink() {
-        this.element.classList.add('miniature');
-        showcase.append(this.element);
-        this.element.style.transform = '';
-    }
-
-    grow() {
+    changeDice() {
         if (!this.classList.contains('miniature')) return;
+        const currentDice = container.firstElementChild; 
+        if (currentDice) {
+            currentDice.classList.add('miniature');
+            currentDice.style.transform = '';
+            currentDice.style.animation = '';
+            showcase.append(currentDice);
+        }
         this.classList.remove('miniature');
         container.append(this);
         this.style.animation = 'PopIn 0.5s linear';
@@ -40,13 +39,12 @@ class Dice {
 
 const showcase = document.getElementById('showcase');
 const container = document.getElementById('container');
-let currentDice, nextDice;
 
 const d6 = document.getElementById('d6');
 const d6Angles = [[0, 0], [0, 90], [90, 0], [270, 0], [0, 270], [0, 180]];
 D6 = new Dice(d6, d6Angles);
 
 const d4 = document.getElementById('d4');
-const d4Angles = [[0, 0], [90, 180], [0, 120], [0, 240]];
+const d4Angles = [[0, 0], [95, 180], [0, 120], [0, 240]];
 
 D4 = new Dice(d4, d4Angles);
